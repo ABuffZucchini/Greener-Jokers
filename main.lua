adf3e3118a9072df5567ea63b4e0fd9af8f390c0
ABuffZucchini <55515577+ABuffZucchini@users.noreply.github.com>
2025-07-15 21:14:26 -0400

    Initial Version

---
+++ main.lua
@@ -0,0 +1,14 @@
+assert(SMODS.load_file("luafiles/ZucchiniJokers.lua"))()
+assert(SMODS.load_file("luafiles/ZucchiniDecks.lua"))()
+
+
+
+
+-- icon loading code
+
+SMODS.Atlas{
+    key = "modicon",
+    path = "znm_icon.png", -- the file in BOTH assets/1x and assets/2x
+    px = 32,
+    py = 32
+}
